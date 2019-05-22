5cfbb82fa6efc0b9f4bd039e15c6ee0d6e064ca1
Gray <285293728@qq.com>
2019-05-22 09:49:33 +0800

    Update SwiftClientTool.py

--- SwiftClientTool.py
+++ SwiftClientTool.py
@@ -52,6 +52,6 @@ class ConnUtil:
         # f = open("mykey.txt")
         # mykey = f.read()
         ConnUtil.con.post_account(headers={"X-Account-Meta-Temp-Url-Key": "mykey"}) # temp account
-        return ConnUtil.end_point+generate_temp_url("/v1/AUTH_test/user_uploads/"+filename,seconds=3600,key=mykey,method='GET')
+        return ConnUtil.end_point+generate_temp_url("/v1/AUTH_test/user_uploads/"+filename,seconds=3600,key="mykey",method='GET')
         f.close()
         
